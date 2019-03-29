ecb4f38bc3583614bb42aba6f242eef016622bf2
Indre Bakeer <indre.bakeer@gmail.com>
2019-03-29 22:41:40 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -2,13 +2,10 @@ module Players
   class Computer < Player
 
     def move(board)
-<<<<<<< HEAD
       if !board.taken?(rand(1..9).to_i)
         rand(1..9).to_s
       end
-=======
 
->>>>>>> f18be614d7d422eae873a588a0fd7b5d670e8030
     end
 
   end
